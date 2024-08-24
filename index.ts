9d5488f39962b4fbf1f6275552301de36a7ac53f
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-08-24 18:32:28 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ import themes from "./src/themes.json";
 const chemical = new ChemicalServer();
 
 const __dirname = path.resolve();
+var http = require("http")
 
 if (!fs.existsSync("build")) {
 	console.log("No build found. Building...");
@@ -54,3 +55,18 @@ chemical.listen(port, () => {
 		);
 	}
 });
+
+
+
+http.createServer(function (req, res) {
+  res.write('alert("Hello World!")'); //write a response to the client
+  res.end(); //end the response
+}).listen(3000); //the server object listens on port 8080
+
+
+
+
+
+
+
+
