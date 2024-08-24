a467ccd8000ff1f416ee16f4e9ddc7cb24cb6e2b
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-08-24 18:36:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 
+var http = require('http');
+http.createServer(function (req, res) {
+  res.write('alert("Hello World!")'); //write a response to the client
+  res.end(); //end the response
+}).listen(3000); //the server object listens on port 8080
+
+
