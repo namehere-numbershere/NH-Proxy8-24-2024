626f58f53bb77cd1b29e5ac4f805e46be09df427
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-08-24 17:29:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(process.env.username);
-}).listen(2000);
+  if (req.url === '/serverinfo') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(process.env.username);
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not Found');
+  }
+}).listen(2000, () => {
+  console.log('Server running at http://localhost:2000');
+});
