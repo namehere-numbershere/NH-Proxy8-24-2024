c21306af55b190af09eca9237bbe0cc1372b3a58
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-08-24 15:39:01 -0400

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+import {discord } from "../settings";
+import { Obfuscated } from "../util/obfuscate";
+
+function Footer() {
+    return (
+        <footer class="footer px-7 mt-auto mb-5 select-none flex items-center justify-between">
+            <div>
+                <Obfuscated>Made By: Nameherenumbershere</Obfuscated>
+            </div>
+            <div class="flex items-center justify-center gap-1.5">
+                
+                <span>/</span>
+                <a class="hover:underline" href={discord}><Obfuscated>Discord</Obfuscated></a>
+                
+            </div>
+        </footer>
+    )
+}
+
+export { Footer };
---
+++ src/settings.ts
@@ -0,0 +1,8 @@
+const obfuscate = true;
+const rammerhead = true;
+const github = "";
+const discord = "https://discord.gg/4SGpW5dM";
+const privacyPolicyUpdated = 0;
+const gamesCdn = "";
+
+export { obfuscate, rammerhead, github, discord, privacyPolicyUpdated, gamesCdn };
